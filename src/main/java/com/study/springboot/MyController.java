1f569495476847def1787a6bd61affdc048e4734
ssujiny <aldebaran97@naver.com>
2024-12-03 23:30:51 +0900

    myBatis simpleBoard

--- src/main/java/com/study/springboot/MyController.java
+++ src/main/java/com/study/springboot/MyController.java
@@ -4,24 +4,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import com.study.springboot.jdbc.IMyUserDao;
+import com.study.springboot.dao.ISimpleBbsDao;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
 	
 	@Autowired
-	private IMyUserDao userDao;
+	ISimpleBbsDao dao;
 	
 	@GetMapping("/")
-	public @ResponseBody String root() throws Exception {
-		return "MyBatis 사용하기";
+	public String root() throws Exception {
+		// MyBatis : SimpleBBS
+		return "redirect:list";
 	}
 	
-	@GetMapping("/user")
+	@GetMapping("/list")
 	public String userListPage(Model model) {
-		model.addAttribute("users", userDao.list());
-		return "userList";
+		model.addAttribute("list", dao.listDao());
+		return "list";
+	}
+	
+	@GetMapping("/view")
+	public String view(HttpServletRequest req, Model model) {
+		String sid = req.getParameter("id");
+		model.addAttribute("dto", dao.viewDao(sid));
+		return "view";
+	}
+	
+	@GetMapping("/writeForm")
+	public String writeForm() {
+		return "writeForm";
+	}
+	
+	@PostMapping("/write")
+	public String write(HttpServletRequest req, Model model) {
+		dao.writeDao(
+			req.getParameter("writer"),
+			req.getParameter("title"),
+			req.getParameter("content")
+		);
+		
+		return "redirect:list";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(HttpServletRequest req, Model model) {
+		dao.deleteDao(req.getParameter("id"));
+		return "redirect:list";
 	}
 }
